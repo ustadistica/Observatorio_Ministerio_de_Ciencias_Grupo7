2869c6e8a2fd14565651f58cdc18d3efd35f6589
camiloacr1322 <camilocast2213@gmail.com>
2025-09-17 23:31:07 -0500

    Update codigo_dimensiones_C.R

--- tarea_dimensiones_y_hechos/codigo_dimensiones_C.R
+++ tarea_dimensiones_y_hechos/codigo_dimensiones_C.R
@@ -1,5 +1,6 @@
 library(readxl)
 library(openxlsx)
+library(dplyr)
 
 Investigadores_Consolidado <- read_excel("Downloads/Investigadores_Consolidado.xlsx")
 
@@ -9,14 +10,18 @@ colnames(Investigadores_Consolidado)
 
 ##  ID del investigador
 
-IDinvestiagores<-Investigadores_Consolidado[,c(4)]
+IDinvestiagores<-unique(Investigadores_Consolidado[,c(4)])
 colnames(IDinvestiagores)
 
 write.xlsx(IDinvestiagores, "Dimension_invetigadores.xlsx")
 
 ##  Cod_Dane del municipio de nacimiento del investigador
 
-IDMUNICIPIO_NAC_PR<-Investigadores_Consolidado[,c(10:14)]
+
+IDMUNICIPIO_NAC_PR <- Investigadores_Consolidado[,c(14,10:13)] %>%
+  distinct(COD_DANE_NAC_PR, .keep_all = TRUE)
+
+
 colnames(IDMUNICIPIO_NAC_PR)
 
 write.xlsx(IDMUNICIPIO_NAC_PR, "Dimension_municipos_de_naciminetos.xlsx")
@@ -25,7 +30,11 @@ write.xlsx(IDMUNICIPIO_NAC_PR, "Dimension_municipos_de_naciminetos.xlsx")
 ## ID de la convocatoria 
 
 
-IDCONVOCATORIA<-Investigadores_Consolidado[,c(1:3)]
+IDCONVOCATORIA<-Investigadores_Consolidado[,c(1:3)] %>%
+  distinct(ID_CONVOCATORIA, .keep_all = TRUE)
+
 colnames(IDCONVOCATORIA)
 
 write.xlsx(IDCONVOCATORIA, "Dimension_convocatoria.xlsx")
+
+
